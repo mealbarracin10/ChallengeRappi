9e7d6c954d2813c325e79b0bc938e7700ec1083b
Martin  Albarracin <edgar3280@gmail.com>
2017-11-07 22:14:58 -0500

    add functionality set query and update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div class="container">
+  <span id="title">Challenge Backend Rappi</span>
+  <form v-on:submit.prevent="sendRequest">
+  <div class="row" id="myApp">
+    <div class="col-lg-3 col-md-3 col-xs-3">
+      <v-select :on-change="changedValue" :options="['Query','Update']"></v-select>
+    </div>
+    <slot name="item" v-for="row in rows">
+    <div :class="row.property">
+		  <input class="form-control" id="add-name"  :placeholder="row.placeholder" required/>
+	  </div>
+    </slot>
   </div>
-</template>
+  <br/>
+  <div class="col-lg-offset-8" id="send">
+		<button type="submit" class="btn btn-primary">Agregar</button>
+	</div>
+  </form>
+  <div id="instructions">
+  <strong>Instructions</strong>
+  <p>This is a paragraph.</p>
+  </div>
+</div>  
 
+</template>
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+import vSelect from "vue-select"
+  export default {
+    components: {vSelect},
+
+    data() {
+      return {
+        rows: []
+      }
+    },
+    methods: {
+      changedValue: function(value) {
+        if(value === 'Query')
+        {
+          this.rows= []
+          this.rows.push({ placeholder: "x1", property: "col-lg-1"}, 
+                         { placeholder: "y1", property: "col-lg-1"}, 
+                         { placeholder: "z1", property: "col-lg-1"},
+                         { placeholder: "x2", property: "col-lg-1"}, 
+                         { placeholder: "y2", property: "col-lg-1"}, 
+                         { placeholder: "z2", property: "col-lg-1"});
+        }
+        else if(value === 'Update')
+        {
+          this.rows= []
+          this.rows.push(
+            { placeholder: "x", property: "col-lg-2"}, 
+            { placeholder: "y", property: "col-lg-2"}, 
+            { placeholder: "z", property: "col-lg-2"},
+            { placeholder: "w", property: "col-lg-2"});
+        }
+          
+  },
+  sendRequest(e){
+      console.log(e)
+  }
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
